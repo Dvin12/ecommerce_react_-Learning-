172573c1d20058891136b3038ce30f35303fe08e
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-09 20:10:03 +0300

    Set up react router, made the nav bar sticky

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React from "react";
 import Home from "./pages/home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-function App() {
+import Cart from "./pages/Cart";
+
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+
+function Layout() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <Outlet></Outlet>
       <Footer></Footer>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { CgShoppingCart } from "react-icons/cg";
 function Header() {
   return (
-    <header className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont">
+    <header className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex justify-between items-center">
         <div>
           <h1
