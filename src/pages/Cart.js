558d2687ff76ab0e509caef001b39d951f601f64
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-05-11 18:44:00 +0300

    added stripe

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,13 @@ import { useSelector } from "react-redux";
 import CartItem from "../components/CartItem";
 import { ToastContainer, toast } from "react-toastify";
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 function Cart() {
   const productData = useSelector((state) => state.store.productData);
+  const userInfo = useSelector((state) => state.store.userInfo);
   const [amount, setAmount] = useState("");
+  const [payNow, setPayNow] = useState();
 
   useEffect(() => {
     let price = 0;
@@ -15,6 +18,14 @@ function Cart() {
     });
     setAmount(price.toFixed(2));
   }, [productData]);
+
+  const handleCheckOut = () => {
+    if (userInfo) {
+      setPayNow(true);
+    } else {
+      toast.error("Please sign in to make an order");
+    }
+  };
   return (
     <div>
       <div className="max-w-screen-xl flex mx-auto py-20">
@@ -37,9 +48,25 @@ function Cart() {
           <p className="font-semibold flex justify-between mt-6">
             Total <span className="text-xl font-bold">${amount}</span>
           </p>
-          <button className="bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-300">
+          <button
+            onClick={handleCheckOut}
+            className="bg-black text-white w-full py-3 mt-6 hover:bg-gray-800 duration-300"
+          >
             Proceed To Checkout
           </button>
+          {payNow && (
+            <div className="w-full mt-6 flex items-center justify-center">
+              <StripeCheckout
+                stripeKey="pk_test_51N6bYMC3vJT26DRMcup97XlAwtC6kbBKbysnBTghZysC0ybS9svFWWijVc8lMA86tlguFDvK5iMhtq9ozoux0ijQ00mIPlulaI"
+                name="Ecomerence dummy store"
+                amount={amount * 100}
+                label="Pay to Ecomerence"
+                description={`Your payment amount is $${amount}`}
+                // token={payment}
+                email={userInfo.email}
+              ></StripeCheckout>
+            </div>
+          )}
         </div>
       </div>
       <ToastContainer
